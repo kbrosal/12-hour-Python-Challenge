f7b3597b12ac90d96ed3c35f8bd372748823d247
kbrosal <kerlyboyrosal@gmail.com>
2023-12-04 20:18:49 +0800

    add basic calculator

---
+++ basic_calc.py
@@ -0,0 +1,23 @@
+def calculate():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ")
+
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Invalid operation"
+    
+print(f"Results: {calculate()}")
+
+
+
+
